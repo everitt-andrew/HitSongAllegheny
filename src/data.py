f5af8f236407c994901f1fb4ddfdbede6e0a339e
Andrew Everitt <everitta@allegheny.edu>
2019-11-05 12:45:30 -0500

    fieldnames set

--- src/data.py
+++ src/data.py
@@ -3,3 +3,6 @@ import csv
 #import spotipy
 
 file = "../data/musiclist.csv"
+with open(file, 'w') as csvfile:
+    fieldnames = ['week', 'position', 'title', 'artist', 'features', 'url', 'genre', 'length', 'explicit', 'loudness', 'l_confidence', 'tempo', 't_confidence', 'key', 'k_confidence']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
