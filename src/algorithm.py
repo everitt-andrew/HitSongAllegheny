c209bd0ea0aff8e38251ffd1376bde6440d847fb
Andrew Everitt <everitta@allegheny.edu>
2020-01-14 11:21:42 -0500

    csv reader added

--- src/algorithm.py
+++ src/algorithm.py
@@ -1,6 +1,19 @@
 #import statements
 import csv
-import scipy
+from sklearn.neural_networkimport MLPClassifier
 import matplotlib
 
 file = "../data/musiclist.csv"
+
+#reading the csv file
+with open(file, 'rb') as f:
+    reader = csv.reader(f)
+    try:
+        #testing csv is read correctly
+        for row in reader:
+            print(row)
+    except csv.Error as e:
+        sys.exit('file %s, line %d: %s' % (file, reader.line_num, e))
+
+#neural network settings
+solver='adam'
